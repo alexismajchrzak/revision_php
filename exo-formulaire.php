522f6db8df7091c7d770141be05e7a226d7608a1
AxelDemorest <axel.demorest@gmail.com>
2022-07-05 17:03:37 +0200

    exo form alexis

---
+++ exo-formulaire.php
@@ -0,0 +1,11 @@
+<form action="" method="POST" class="mt-4">
+    <div class="mb-3 bg-white rounded shadow-sm border border-2 p-4">
+        <label for="lastName" class="form-label text-muted text-uppercase ms-1" style="font-size:12px;opacity:0.5">Titre de votre sujet</label>
+        <input name="lastName" type="text" class="form-control" id="lastName" placeholder="Quel est le titre de votre sujet ?" required>
+    </div>
+
+    <div class="mb-3 bg-white rounded shadow-sm border border-2 p-4">
+        <label for="firstName" class="form-label text-muted text-uppercase ms-1" style="font-size:12px;opacity:0.5">Titre de votre sujet</label>
+        <input name="firstName" type="text" class="form-control" id="firstName" placeholder="Quel est le titre de votre sujet ?" required>
+    </div>
+</form>
